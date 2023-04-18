e87a8a6f2067d72d71bac8a97dce30258ae3dc8f
takuya <task5622@gmail.com>
2023-04-17 22:42:56 -0700

    fix type

---
+++ apps/frontend/app/profile/layout.tsx
@@ -0,0 +1,34 @@
+'use client'
+/* eslint-disable no-constant-condition */
+// import { ApolloProvider } from '@apollo/client'
+// import { ColorScheme, ColorSchemeProvider } from '@mantine/core'
+import Head from 'next/head'
+import React, { PropsWithChildren } from 'react'
+// import { layoutApolloClient } from '../../apollo-client'
+// import Layout from '../component/layout/Layout'
+// import WalletConnect from '../component/walletConnect/WalletConnectModal'
+// import '../libs/firebase'; // Initialize FirebaseApp
+// import RecoilProvider from '../recoil/RecoilProvider'
+// import '../styles/globals.css'
+// import '../styles/player.css'
+// import RootStyleRegistry from './emotion'
+
+const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  // const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
+
+  // const toggleColorScheme = (value?: ColorScheme) =>
+  //   console.log(value || ('dark' === 'dark' ? 'dark' : 'dark'))
+  // both en and jp
+  return (
+    <html lang="en">
+      <Head>
+        <title>Next.js</title>
+      </Head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ apps/frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// app/page.tsx
+/** @jsxImportSource @emotion/react */
+'use client'
+
+import { AppShell, Notification } from '@mantine/core'
+// import { useAccount } from 'wagmi'
+import React from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+// import { refreshAuth } from '../../libs/authentication/refresh'
+// import { RegisterArtistProfile } from '../layout/RegisterArtistProfile'
+// import WalletConnectModal from '../walletConnect/WalletConnectModal'
+import { AppHeader } from './AppHeader'
+import { FooterLinks } from './Footer'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   const { address, isConnected } = useAccount()
+//   const profiles = useGetProfileByAddress(address)
+//   const [isRegistered, setIsregistered] = useRecoilState(
+//     LensIsAritistRegisterdState
+//   )
+
+//   const isLoading = useRecoilValue(LensAuthLoadingState)
+
+  return (
+    <>
+      <AppShell
+        padding="md"
+        // navbar={<AppNavBar />}
+        header={<AppHeader />}
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        {children}
+        <FooterLinks />
+      </AppShell>
+    </>
+  )
+}
+
+export default Layout
+// const Test = () => {
+//   const { data } = useDefaultProfile()
+//   console.log(data, 'datatata')
+//   return(<></>)
+// }
